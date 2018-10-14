7758324dedd43ff8083b78c0ce578bb1866735f7
techcoderx <engtianxi@outlook.com>
2018-10-15 07:52:05 +0800

    remove mv
    not needed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Multer = require('multer');
 const Shell = require('shelljs');
 const fs = require('fs');
-const mv = require('mv');
 const Config = require('./config.json');
 const Express = require('express');
 const app = Express();
