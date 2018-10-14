1b58587e08c0947a6a71367485fae4024d0949b3
techcoderx <engtianxi@outlook.com>
2018-10-14 18:31:26 +0800

    start of uploader interface

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const Config = require('./config.json');
 const Express = require('express');
 const app = Express();
 
+app.use(Express.static('public'));
+
+app.get('/', (request,response) => {
+    fs.readFile('./uploader.html',function(error, data) {
+        if (error != null) {
+            response.writeHead(404);
+            response.write(error);
+            response.end();
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data);
+            response.end();
+        }
+    });
+});
+
+app.listen(Config.port);
