3a5a99346a14f265eaadf559a0c715b36d7c75a4
techcoderx <engtianxi@outlook.com>
2018-10-18 08:54:07 +0800

    Broadcast to post on Steem

---
+++ dtube-sprite.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo
+	echo call: $0 input_file.mp4 output_file.jpg
+	exit 1
+fi
+
+if [ "$(command -v ffmpeg)" = "" ]; then
+	echo
+	echo Error: FFmpeg not found
+	exit 1
+fi
+
+if [ "$(command -v montage)" = "" ]; then
+	echo
+	echo Error: ImageMagick not found
+	exit 1
+fi
+
+if [ ! -e $1 ]; then
+	echo
+	echo Error: Input File not found
+	exit 1
+fi
+
+TEMPDIR=$(mktemp -d)
+
+RUNTIME=$(ffprobe -hide_banner -loglevel error -select_streams v:0 -show_streams $1 2>/dev/null | grep duration= | sed -e 's/duration=//')
+RUNTIME=${RUNTIME%.*}
+
+if [ $RUNTIME -lt 100 ]; then
+	MAX=$RUNTIME
+	STEPS=1
+else
+	MAX=100
+	STEPS=$(bc <<< "scale=2; $RUNTIME/100")
+fi
+
+printf "Extracting frames from source video "
+
+for i in `seq 0 $((MAX - 1))` ; do
+	lz=""
+	if [ $i -lt 10 ]; then lz=0; fi
+
+	ffmpeg -hide_banner -loglevel error -accurate_seek -ss $(bc <<< $i*$STEPS) -i $1 -s 210x118 -frames:v 1 $TEMPDIR/sprite_$lz$i.bmp
+
+	if [ "$((i%(MAX/4)))" = 0 ]; then printf "$((i/(MAX/4)*25))%% "; fi
+done
+printf "100%%\nCreating sprite image\n"
+
+montage $TEMPDIR/sprite_*.bmp -mode Concatenate -tile 1x$MAX $2
+
+echo Done.
+echo Video duration: $RUNTIME seconds
+
+rm -rf $TEMPDIR
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const SteemConnect = require('steemconnect');
 const Multer = require('multer');
 const Shell = require('shelljs');
+const getDuration = require('get-video-duration');
 const fs = require('fs');
 const Config = require('./config.json');
 const Express = require('express');
@@ -22,6 +23,7 @@ if (Config.port == NaN) {
 app.use(Express.static('public'));
 
 app.get('/', (request,response) => {
+    // Welcome page
     fs.readFile('./welcome.html',function(error, data) {
         if (error != null) {
             response.writeHead(404);
@@ -36,6 +38,7 @@ app.get('/', (request,response) => {
 });
 
 app.get('/upload', (request,response) => {
+    // Uploader page
     if (request.query.access_token != "") {
         var sc2api = SteemConnect.Initialize({ accessToken: request.query.access_token });
         sc2api.me(function(err,res) {
@@ -76,20 +79,133 @@ app.post('/videoupload', cpUpload, function (request,response) {
         snapPathName = request.files.SnapUpload[0].path + '.png';
         fs.renameSync(request.files.SnapUpload[0].path,request.files.SnapUpload[0].path + '.png');
     }
+    
+    var sc2token = request.body.accesstoken;
+    var api = SteemConnect.Initialize({ accessToken: sc2token });
+    
+    // Get username
+    api.me(function(err,res) {
+        let username = res.account.name;
 
-    console.log(request.body.accesstoken);
-    // Add files to IPFS
-    Shell.exec('ipfs add ' + videoPathName,function(code,stdout,stderr) {
-        var outs = stdout.split(' ');
-        var ipfsHash = outs[1];
-        Shell.exec('ipfs pin add ' + ipfsHash);
-        Shell.exec('ipfs add ' + snapPathName,function(code,stdout,stderr) {
-            var snapOuts = stdout.split(' ');
-            var ipfsSnapHash = snapOuts[1];
-            Shell.exec('ipfs pin add ' + ipfsSnapHash);
-            response.end('It works?');
+        // Add files to IPFS
+        Shell.exec('ipfs add ' + videoPathName,function(code,stdout,stderr) {
+            var outs = stdout.split(' ');
+            var ipfsHash = outs[1];
+            Shell.exec('ipfs pin add ' + ipfsHash);
+            Shell.exec('ipfs add ' + snapPathName,function(code,stdout,stderr) {
+                var snapOuts = stdout.split(' ');
+                var ipfsSnapHash = snapOuts[1];
+                Shell.exec('ipfs pin add ' + ipfsSnapHash);
+
+                // Sprite creation
+                Shell.exec('./dtube-sprite.sh ' + videoPathName + ' ' + request.files.VideoUpload[0].path + '.jpg');
+                Shell.exec('ipfs add ' + request.files.VideoUpload[0].path + '.jpg',function(code,stdout,stderr) {
+                    var spriteouts = stdout.split(' ');
+                    var ipfsSpriteHash = spriteouts[1];
+                    Shell.exec('ipfs pin add ' + ipfsSpriteHash);
+
+                    // Get video duration and file size
+                    var videoSize = request.files.VideoUpload[0].size;
+                    getDuration(videoPathName).then((videoDuration) => {
+                        // Post on blockchain
+                        let metadata = request.body;
+                        let transaction = generatePost(username,ipfsHash,ipfsSnapHash,ipfsSpriteHash,metadata.title,metadata.description,metadata.tags,videoDuration,videoSize,request.body.powerup);
+                        api.broadcast(transaction,function(err) {
+                            if (err != null) {
+                                response.end('Error: '+ err);
+                                return;
+                            }
+                            console.log('It works!!! Video posted onto DTube!')
+                            response.end('Video upload success!');
+                        });
+                    });
+                });
+            });
         });
     });
 });
+
+function generatePermlink() {
+    var permlink = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 8; i++) {
+        permlink += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return permlink;
+}
+
+function buildPostBody(author,permlink,videoHash,snapHash,description) {
+    return '<center><a href=\'https://d.tube/#!/v/' + author + '/' + permlink + '\'><img src=\'https://ipfs.io/ipfs/' + snapHash + '\'></a></center><hr>\n\n' + description + '\n\n<hr><a href=\'https://d.tube/#!/v/' + author + '/' + permlink + '\'> ▶️ DTube</a><br /><a href=\'https://ipfs.io/ipfs/' + videoHash + '\'> ▶️ IPFS</a>'
+}
+
+function buildJsonMetadata(sourceHash,snapHash,spriteHash,title,description,tagString,duration,filesize,author,permlink) {
+    var DTubeTags = tagString.split(' ');
+    var SteemTags = DTubeTags;
+    SteemTags.push('dtube');
+
+    var jsonMeta = {
+        video: {
+            info: {
+                title: title,
+                snaphash: snapHash,
+                author: author,
+                permlink: permlink,
+                duration: duration,
+                filesize: filesize,
+                spritehash: spriteHash,
+            },
+            content: {
+                videohash: sourceHash,
+                description: description,
+                tags: DTubeTags,
+            },
+        },
+        tags: SteemTags,
+        app: Config.JSONApp,
+    }
+    return jsonMeta;
+}
+
+function generatePost(username,sourceHash,snapHash,spriteHash,title,description,tagString,duration,filesize,powerUp) {
+    let permlink = generatePermlink();
+    let tags = tagString.split(' ');
+
+    // Power up all rewards or not
+    var percentSBD = 10000;
+    if (powerUp == 'on') {
+        percentSBD = 0;
+    }
+
+    let operations = [
+        [ 'comment', {
+                parent_author: '',
+                parent_permlink: tags[0],
+                author: username,
+                permlink: permlink,
+                title: title,
+                body: buildPostBody(username,permlink,sourceHash,snapHash,description),
+                json_metadata: JSON.stringify(buildJsonMetadata(sourceHash,snapHash,spriteHash,title,description,tagString,duration,filesize,username,permlink)),
+            }
+        ],
+        [ "comment_options", {
+            author: username,
+            permlink: permlink,
+            max_accepted_payout: '1000000.000 SBD',
+            percent_steem_dollars: percentSBD,
+            allow_votes: true,
+            allow_curation_rewards: true,
+            extensions: [
+                [0, {
+                    beneficiaries: [{
+                        account: 'dtube',
+                        weight: 1000
+                    }]
+                }]
+            ]
+        }]
+    ];
+    return operations;
+}
 
 app.listen(Config.port);
