cbe75e718f46e594f38e9d5ea89871d63368f160
techcoderx <engtianxi@outlook.com>
2020-03-14 18:02:03 +0800

    add tusd installation scripts

---
+++ scripts/tusd_macos_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# tusd installation script for macOS
+
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+
+mkdir tusd && cd tusd
+echo "Downloading tusd for macOS..."
+wget -q https://github.com/tus/tusd/releases/download/v1.1.0/tusd_darwin_amd64.zip
+unzip tusd_darwin_amd64.zip
+cd tusd_darwin_amd64
+mv tusd /usr/local/bin
+cd ../..
+rm -r tusd
+echo "tusd installed successfully!"
---
+++ scripts/tusd_ubuntu_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# tusd installation script for Ubuntu
+
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+
+mkdir tusd && cd tusd
+echo "Downloading tusd for Ubuntu..."
+wget -q https://github.com/tus/tusd/releases/download/v1.1.0/tusd_linux_amd64.tar.gz
+tar -xvf tusd_linux_amd64.tar.gz
+cd tusd_linux_amd64
+mv tusd /usr/bin
+cd ../..
+rm -r tusd
+echo "tusd installed successfully!"
