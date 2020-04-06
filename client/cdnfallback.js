761498ae56f5c86c01eeb80c2f38ceec5dffc024
techcoderx <engtianxi@outlook.com>
2020-04-06 17:53:29 +0800

    add cdn fallback

---
+++ client/cdnfallback.js
@@ -0,0 +1,16 @@
+libs = []
+
+switch (window.location.pathname) {
+    case '/':
+        libs = ['axios','javalon','hivesigner']
+        break
+    case '/reviews':
+        libs = ['axios','javalon','hivesigner']
+        break
+    case '/upload':
+        libs = ['axios','javalon','async','io','hivesigner','steem','HtmlSanitizer','tus','moment']
+        break
+}
+
+for (let i = 0; i < libs.length; i++)
+    if (!window[libs[i]]) document.write('<script src="/lib/' + libs[i] + '.min.js"><\/script>')
