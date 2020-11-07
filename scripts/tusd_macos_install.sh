9026ec475db34bb9dac6f4ae00cab84e885a2d51
techcoderx <engtianxi@outlook.com>
2020-11-07 12:08:48 +0800

    update tusd install scripts

--- scripts/tusd_macos_install.sh
+++ scripts/tusd_macos_install.sh
@@ -8,7 +8,7 @@ fi
 
 mkdir tusd && cd tusd
 echo "Downloading tusd for macOS..."
-wget -q https://github.com/tus/tusd/releases/download/v1.3.0/tusd_darwin_amd64.zip
+wget -q https://github.com/tus/tusd/releases/download/v1.4.0/tusd_darwin_amd64.zip
 unzip tusd_darwin_amd64.zip
 cd tusd_darwin_amd64
 mv tusd /usr/local/bin
