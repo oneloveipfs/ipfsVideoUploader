4dfacfb1d40d71c429b6d426f72e656202dabc8f
techcoderx <engtianxi@outlook.com>
2021-12-11 20:13:53 +0800

    update tusd installers

--- scripts/tusd_macos_install.sh
+++ scripts/tusd_macos_install.sh
@@ -6,11 +6,22 @@ if [ `whoami` != root ]; then
     exit
 fi
 
+ARCH="$(uname -m)"
+if [ $ARCH == "x86_64" ]; then
+    if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
+        echo "Seems that you are running this in Rosetta 2. Please run this natively in arm64."
+        exit
+    else
+        ARCH="amd64"
+    fi 
+fi
+
 mkdir tusd && cd tusd
 echo "Downloading tusd for macOS..."
-wget -q https://github.com/tus/tusd/releases/download/v1.4.0/tusd_darwin_amd64.zip
-unzip tusd_darwin_amd64.zip
-cd tusd_darwin_amd64
+echo "Architecture: ${ARCH}"
+wget -q https://github.com/tus/tusd/releases/download/v1.8.0/tusd_darwin_${ARCH}.zip
+unzip tusd_darwin_${ARCH}.zip
+cd tusd_darwin_${ARCH}
 mv tusd /usr/local/bin
 cd ../..
 rm -r tusd
--- scripts/tusd_windows_install.ps1
+++ scripts/tusd_windows_install.ps1
@@ -1,5 +1,5 @@
 #Requires -RunAsAdministrator
-Invoke-WebRequest -o tusd.zip "https://github.com/tus/tusd/releases/download/v1.4.0/tusd_windows_amd64.zip"
+Invoke-WebRequest -o tusd.zip "https://github.com/tus/tusd/releases/download/v1.8.0/tusd_windows_amd64.zip"
 Expand-Archive -LiteralPath tusd.zip -DestinationPath tusd
 Set-Location tusd/tusd*
 Copy-Item tusd.exe C:\Windows\System32
