885ce91c79beea266c4c5cd50628752c0e04714b
techcoderx <engtianxi@outlook.com>
2021-12-16 11:07:46 +0800

    desktop app build scripts

---
+++ scripts/build-full.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/oneloveipfs/ipfsVideoUploader oneloveipfs-full
+cd oneloveipfs-full
+npm i
+cp config_example.json config.json
+npm run test
+npm run buildinstaller
---
+++ scripts/build-remote.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/oneloveipfs/ipfsVideoUploader oneloveipfs-remote
+cd oneloveipfs-remote
+npm i
+cp config_example.json config.json
+REMOTE_APP=1 npm run prepapp
+npm run dep-prune
+npm run buildinstaller
