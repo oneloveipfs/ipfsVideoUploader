673db298ed8627131b858e22e7d89dc7844aa79e
techcoderx <37140751+techcoderx@users.noreply.github.com>
2022-08-06 15:06:00 +0800

    append thumbnaulUrl

--- client/uploader.js
+++ client/uploader.js
@@ -1018,6 +1018,7 @@ function buildJsonMetadataAvalon() {
                 }
             }
         },
+        thumbnailUrl: config.gateway+'/ipfs/'+postparams.imghash,
         dur: postparams.duration,
         title: postparams.title,
         desc: postparams.description,
