4cbbc8e62e84093e7cadf4435ef26bb8e82eaef4
techcoderx <engtianxi@outlook.com>
2022-10-15 15:39:49 +0800

    more settings tests

--- test/dbTest.js
+++ test/dbTest.js
@@ -89,6 +89,9 @@ describe('Database',() => {
 
     it('uplThreads settings validator',(done) => {
         assert.isNotNull(db.settingsValidator.uplThreads('a'))
+        assert.isNotNull(db.settingsValidator.uplThreads(true))
+        assert.isNotNull(db.settingsValidator.uplThreads(false))
+        assert.isNotNull(db.settingsValidator.uplThreads(null))
         assert.isNotNull(db.settingsValidator.uplThreads(0))
         assert.isNotNull(db.settingsValidator.uplThreads(51))
         assert.isNull(db.settingsValidator.uplThreads(1))
@@ -104,6 +107,10 @@ describe('Database',() => {
         assert.isNotNull(db.settingsValidator.descTemplate('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Na'))
         // 1,000 characters
         assert.isNull(db.settingsValidator.descTemplate('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. N'))
+        // 1 character
+        assert.isNull(db.settingsValidator.descTemplate('a'))
+        // 0 characters
+        assert.isNull(db.settingsValidator.descTemplate(''))
         done()
     })
 
